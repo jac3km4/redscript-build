c1706427e246fd6ed41830e39ffa73d2d1c0e29b
jekky <11986158+jac3km4@users.noreply.github.com>
2024-02-10 15:15:55 +0000

    fix: CI script fixes

--- build.ps1
+++ build.ps1
@@ -1,15 +1,15 @@
 
 # sample scripts for compiler instrumentation
 $samples = @(
-	'djkovrik-mods\src\Limited HUD\r6\scripts',
-	'djkovrik-mods\src\Always First Equip\r6\scripts',
-	'djkovrik-mods\src\Muted Markers\r6\scripts'
+	'djkovrik-mods/src/Limited HUD/r6/scripts',
+	'djkovrik-mods/src/Always First Equip/r6/scripts',
+	'djkovrik-mods/src/Muted Markers/r6/scripts'
 )
 
 # script cache file used for compilation
 if (!(Test-Path .\data\cache\final.redscripts)) {
 	mkdir -p .\data\cache
-	curl 'https://oshi.at/LUYQ/final.redscripts.bk' -O  '.\data\cache\final.redscripts'
+	curl 'https://oshi.at/LUYQ/final.redscripts.bk' -O  './data/cache/final.redscripts'
 }
 
 Push-Location redscript
@@ -17,7 +17,7 @@ Push-Location redscript
 cargo pgo build -- -p scc-lib
 cargo pgo build -- -p scc
 foreach ($sample in $samples) {
-	cargo pgo run -- -p scc -- -compile "..\samples\$sample" -customCacheDir '..\data\cache'
+	cargo pgo run -- -p scc -- -compile "../samples/$sample" -customCacheDir '../data/cache'
 }
 cargo pgo optimize build -- -p scc-lib
 
